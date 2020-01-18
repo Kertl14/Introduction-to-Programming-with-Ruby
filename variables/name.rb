b9c5561741dd263b20f399461af15d2745a2803b
Kevin Ertl <kevin.g.ertl@gmail.com>
2020-01-18 23:53:11 +0000

    Adding final update to name.rb

--- variables/name.rb
+++ variables/name.rb
@@ -1,6 +1,8 @@
-puts "What is your name? "
-name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp
+puts "What is your last name? "
+last_name = gets.chomp
 
-puts "Hello #{name}"
+puts "Hello #{first_name} #{last_name}"
 
-10.times { |x| puts name }
+10.times { |x| puts "#{first_name} #{last_name}" }
