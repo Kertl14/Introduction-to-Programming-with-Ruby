9060ffa0ddfc111056fd6b4a3e7269420bc1ee8c
Kevin Ertl <kevin.g.ertl@gmail.com>
2020-01-18 23:43:29 +0000

    Adding age.rb

---
+++ variables/age.rb
@@ -0,0 +1,12 @@
+puts "How old are you? "
+age = gets.chomp.to_i
+
+plus_10 = age + 10
+plus_20 = age + 20
+plus_30 = age + 30
+plus_40 = age + 40
+
+puts "In ten years you will be #{plus_10}"
+puts "In twenty years you will be #{plus_20}"
+puts "In thirty years you will be #{plus_30}"
+puts "In fourty years you will be #{plus_40}"
